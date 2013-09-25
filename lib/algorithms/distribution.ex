9f6edd16155c4235728c9243fc427f2f7061b109
Nae Tennouji <me@require.pm>
2013-09-25 04:23:36 +0400

    Convert tabs to spaces

--- lib/algorithms/distribution.ex
+++ lib/algorithms/distribution.ex
@@ -1,8 +1,8 @@
 defmodule Algorithms.Distribution do
-	
-	def robin([h|t]) do
-		stack = :lists.append(t, [h])
-		{:ok, h, stack}
-	end
+
+    def robin([h|t]) do
+        stack = :lists.append(t, [h])
+        {:ok, h, stack}
+    end
 
 end
